aa441f0ac4d8bc147ebacb820d88e8d37fde4ca8
Vincent Ang <vinceajy93@gmail.com>
2023-01-17 16:01:13 +0800

    completed client code for cookie workshop 4

--- App.java
+++ App.java
@@ -1,5 +1,17 @@
 package sg.edu.nus.iss;
 
+import java.io.BufferedInputStream;
+import java.io.BufferedOutputStream;
+import java.io.Console;
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.EOFException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.Socket;
+import java.net.UnknownHostException;
+
 /**
  * Hello world!
  */
@@ -9,9 +21,38 @@ public final class App {
 
     /**
      * Says hello to the world.
+     * 
      * @param args The arguments of the program.
      */
-    public static void main(String[] args) {
-        System.out.println("Hello World!");
+    public static void main(String[] args) throws UnknownHostException, IOException {
+        Socket socket = new Socket("10.244.48.253", 3000);
+
+        try (OutputStream os = socket.getOutputStream()) {
+            BufferedOutputStream bos = new BufferedOutputStream(os);
+            DataOutputStream dos = new DataOutputStream(bos);
+
+            Console con = System.console();
+            String readInput = "", receivedMessage ="";
+
+            try (InputStream is = socket.getInputStream()) {
+                BufferedInputStream bis = new BufferedInputStream(is);
+                DataInputStream dis = new DataInputStream(bis);
+
+                while (!readInput.equalsIgnoreCase("close")) {
+                    readInput = con.readLine("Enter a command: ");
+                    dos.writeUTF(readInput);
+                    dos.flush();
+
+                    receivedMessage = dis.readUTF();
+                    System.out.printf("cookie received: %s\n",receivedMessage);
+                }
+
+                dis.close();
+                bis.close();
+                is.close();
+            }
+        } catch (EOFException ex) {
+            socket.close();
+        }
     }
 }
